ca12d25291da83a4c3693b331a856ff0dfca88b9
Prof. Hebert <hebertphp@gmail.com>
2025-06-05 21:09:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-document.getElementById('mostrarMapa').addEventListener('click', () => {
-  const divMapa = document.getElementById('mapa');
-  divMapa.style.display = 'block';
+window.onload = () => {
+  window.mapa = L.map('mapa').setView([-23.55052, -46.63331], 13); // São Paulo
 
-  if (!window.mapa) {
-    window.mapa = L.map('mapa').setView([-23.55052, -46.63331], 13); // São Paulo
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(window.mapa);
-  } else {
-    window.mapa.invalidateSize(); // corrige erro se o mapa for reexibido
-  }
-});
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+  }).addTo(window.mapa);
+};
